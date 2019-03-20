cd576d73b73692fe8255cd924242511b9715ef17
RSMJr <journal.indoor@gmail.com>
2019-03-20 01:22:45 -0300

    primeira pagina pronta

--- js/ronyApp.js
+++ js/ronyApp.js
@@ -1,5 +1,11 @@
 
-var app = angular.module('myApp', []);
-app.controller('mainCtrl', function() {
+var app = angular.module('ronyApp', []);
+
+app.controller('mainCtrl', function($http) {
+	var vm = this;
+
+	$http.get('http://private-59658d-celulardireto2017.apiary-mock.com/plataformas').then(function(response){
+		vm.plataformas = response.data.plataformas;
+	})
   
 });
