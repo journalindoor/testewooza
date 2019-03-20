a3b4305b561b60dcb717d2414089cb6b8578ebf3
RSMJr <journal.indoor@gmail.com>
2019-03-20 02:51:35 -0300

    tela do tablet e do computador prontas

--- js/ronyApp.js
+++ js/ronyApp.js
@@ -1,11 +1,20 @@
 
-var app = angular.module('ronyApp', ['ui-router']);
+var app = angular.module('ronyApp', ['ngRoute']);
 
-app.config(function() {
-	$urlRouterProvider.otherwise('/');
+app.config(['$routeProvider', function($routeProvider){
 
+	$routeProvider
+	.when('/', {
+		templateUrl: 'view/home.html'
+	})	
+	.when('/TBT01', {
+		templateUrl: 'view/tablet.html'
+	})
+	.when('/CPT02', {
+		templateUrl: 'view/computador.html'
+	})
 
-});
+}]);
 
 app.controller('mainCtrl', function($http) {
 	var vm = this;
@@ -13,5 +22,13 @@ app.controller('mainCtrl', function($http) {
 	$http.get('http://private-59658d-celulardireto2017.apiary-mock.com/plataformas').then(function(response){
 		vm.plataformas = response.data.plataformas;
 	})
-  
+
+	$http.get('http://private-59658d-celulardireto2017.apiary-mock.com/planos/TBT01').then(function(response){
+		vm.planoTablet = response.data.planos;
+	})
+
+	$http.get('http://private-59658d-celulardireto2017.apiary-mock.com/planos/CPT02').then(function(response){
+		vm.planoComputador = response.data.planos;
+	})
+
 });
