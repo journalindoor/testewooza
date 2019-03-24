526286d7bf0918b095d6f81fa6ff5c238dc13b17
RSMJr <journal.indoor@gmail.com>
2019-03-23 23:36:27 -0300

    Fase final

--- js/ronyApp.js
+++ js/ronyApp.js
@@ -5,12 +5,16 @@ app.config(['$routeProvider', function($routeProvider){
 
 	$routeProvider
 	.when('/', {
-		templateUrl: 'view/home.html',
+		templateUrl: 'view/plataformas.html',
 		controller: 'mainCtrl'
 	})
-	.when('/planos/:skuPlano/:nomePlano', {
+	.when('/planos/:skuPlataforma/:nomePlataforma', {
 		templateUrl: 'view/planos.html',
 		controller: 'planosCtrl'
+	})
+	.when('/dados/:skuPlataforma/:skuPlano', {
+		templateUrl: 'view/dados.html',
+		controller: 'dadosCtrl'
 	});
 	
 
@@ -23,18 +27,46 @@ app.controller('mainCtrl', ['$scope', '$http', '$routeParams', function($scope, 
 		$scope.plataformas = response.data.plataformas;
 	})
 
-}]) 
+}]);
 
 app.controller('planosCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 
-	$scope.skuPlano = $routeParams.skuPlano;
-	$scope.nomePlano = $routeParams.nomePlano;
+	$scope.skuPlataforma = $routeParams.skuPlataforma;
+	$scope.nomePlataforma = $routeParams.nomePlataforma;
 
-	$http.get('http://private-59658d-celulardireto2017.apiary-mock.com/planos/'+$routeParams.skuPlano).then(function(response){
+	$http.get('http://private-59658d-celulardireto2017.apiary-mock.com/planos/'+$routeParams.skuPlataforma).then(function(response){
 		$scope.todosPlanos = response.data.planos;
-		var tdPlanos = response.data.planos;
-		console.log("teste", tdPlanos);
 	})
 
+}]);
 
-}]) 
+app.controller('dadosCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+
+	$scope.skuPlataforma = $routeParams.skuPlataforma;
+	$scope.skuPlano = $routeParams.skuPlano;
+
+
+	$scope.mostraDados = function (){
+		var usunome = $scope.usuNome;
+		var usuemail = $scope.usuEmail;
+		var usunasc = $scope.usuNasc;
+		var usucpf = $scope.usuCpf;
+		var usutel = $scope.usuTel;
+		if(
+			usunome &&
+			usuemail &&
+			usunasc &&
+			usucpf &&
+			usutel
+			){
+			console.log('Nome: ', usunome);
+			console.log('E-mail: ', usuemail);
+			console.log('Nascimento: ', usunasc);
+			console.log('CPF: ', usucpf);
+			console.log('Tel.: ', usutel);
+		} 
+		else {
+			console.log('Tentou usar o formulário sem prencher os dados né?')
+		}
+	}
+}]);
